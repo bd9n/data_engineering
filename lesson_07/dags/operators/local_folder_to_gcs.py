519aa3bd036f784a02e0ce0ea872094dce63f50d
Bohdan Ivakhnov <b.ivakhnov@bossgs.com>
2023-07-14 08:38:18 +0300

    Completed lesson_10

---
+++ lesson_07/dags/operators/local_folder_to_gcs.py
@@ -0,0 +1,30 @@
+import os
+
+from airflow.models import BaseOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+
+
+class UploadFolderToGCSOperator(BaseOperator):
+    def __init__(self, src_folder, dst_bucket, bucket, gcp_conn_id: str = "google_cloud_default", **kwargs):
+        super().__init__(**kwargs)
+        self.bucket = bucket
+        self.src_folder = src_folder
+        self.dst_bucket = dst_bucket
+        self.gcp_conn_id = gcp_conn_id
+
+    def execute(self, context):
+        date = context.get('ds')
+        hook = GCSHook(gcp_conn_id=self.gcp_conn_id)
+        dest_dir = os.path.join(self.src_folder, date)
+
+        for root, dirs, files in os.walk(dest_dir):
+
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                gcs_object = os.path.join(self.dst_bucket, date, file_name)
+                hook.upload(
+                    bucket_name=self.bucket,
+                    object_name=gcs_object,
+                    filename=file_path,
+                )
+                self.log.info("File %s uploaded to %s in %s bucket", file_path, gcs_object, self.bucket)
--- lesson_07/dags/process_sales.py
+++ lesson_07/dags/process_sales.py
@@ -1,13 +1,14 @@
 import os
-from datetime import datetime, timedelta
+from datetime import timedelta
 from airflow import DAG
 
+from operators.local_folder_to_gcs import UploadFolderToGCSOperator
 from operators.get_and_save_fake_api_operator import GetAndSaveFakeApiOperator
 from operators.convert_to_avro_operator import ConvertToAvroOperator
 
 RAW_DIR = '/opt/airflow/data/raw/'
 STG_DIR = '/opt/airflow/data/stg/'
-FAKE_API_TOKEN = os.environ['FAKE_API_TOKEN']
+GCS_DIR = 'stg'
 
 default_args = {
     'start_date': '2022-08-09',
@@ -17,6 +18,7 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
+
 with DAG(
         'process_sales',
         default_args=default_args,
@@ -24,7 +26,7 @@ with DAG(
     extract_data_from_api = GetAndSaveFakeApiOperator(
         task_id='extract_data_from_api',
         http_conn_id='fake_api_http',
-        headers={'Authorization': FAKE_API_TOKEN},
+        headers={'Authorization': os.environ.get('FAKE_API_TOKEN')},
         endpoint='sales',
         raw_dir=RAW_DIR,
         method='GET',
@@ -36,4 +38,12 @@ with DAG(
         stg_dir=STG_DIR
     )
 
-    extract_data_from_api >> convert_to_avro
+    upload_task = UploadFolderToGCSOperator(
+        task_id=f'upload_file',
+        src_folder=STG_DIR,
+        dst_bucket=GCS_DIR,
+        bucket='test_bucket_ivakhnov',
+        gcp_conn_id='gcs-conn-id'
+    )
+
+    extract_data_from_api >> convert_to_avro >> upload_task
